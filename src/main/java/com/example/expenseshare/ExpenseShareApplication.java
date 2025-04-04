891378456050719854d7a412f63db209d4063f81
Sanket Singh <singhsanket143@gmail.com>
2025-04-04 11:32:48 +0530

    Added basic project structure

---
+++ src/main/java/com/example/expenseshare/ExpenseShareApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseShareApplication.class, args);
+    }
+
+}
